99d8907e95c85891e5bdad69f79ac5014e7a6c06
2462264481@qq.com <pengBIN123456>
2020-11-24 14:44:43 +0800

    初始化提交

---
+++ crm-java/src/test/java/com/panelbam/crmjava/CrmJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.panelbam.crmjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
